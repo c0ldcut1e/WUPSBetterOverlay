278574201791c66f011bd7e412f9736ae422b8ef
c0ldcut1e <deinemuddayuki123@gmail.com>
2025-07-17 11:05:01 +0200

    Add .gitignore and cleaned up

--- build/PluginInfo.d
+++
@@ -1,4 +0,0 @@
-PluginInfo.o: \
- /home/coldcutie/Desktop/WiiU/WUPS/WUPSBetterOverlay/src/utils/PluginInfo.cpp \
- /home/coldcutie/Desktop/WiiU/WUPS/WUPSBetterOverlay/src/utils/PluginInfo.h
-/home/coldcutie/Desktop/WiiU/WUPS/WUPSBetterOverlay/src/utils/PluginInfo.h:
--- build/imgui_demo.d
+++
@@ -1,6 +0,0 @@
-imgui_demo.o: \
- /home/coldcutie/Desktop/WiiU/WUPS/WUPSBetterOverlay/imgui/imgui_demo.cpp \
- /home/coldcutie/Desktop/WiiU/WUPS/WUPSBetterOverlay/imgui/imgui.h \
- /home/coldcutie/Desktop/WiiU/WUPS/WUPSBetterOverlay/imgui/imconfig.h
-/home/coldcutie/Desktop/WiiU/WUPS/WUPSBetterOverlay/imgui/imgui.h:
-/home/coldcutie/Desktop/WiiU/WUPS/WUPSBetterOverlay/imgui/imconfig.h:
